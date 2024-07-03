c446d49eb1c233b53462664b3c443880d9ba6920
Edoardo Protti <edoardo.protti@gmail.com>
2024-07-03 19:56:16 +0200

    implemented live stream hand recognizer

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 import cv2
@@ -5,17 +6,23 @@ import mediapipe as mp
 import numpy as np
 
 BaseOptions = mp.tasks.BaseOptions
-GestureRecognizer = mp.tasks.vision.GestureRecognizer
-GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
-GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+landmark_buffer = np.zeros((21, 2), dtype=np.int32)
 
-# Create a gesture recognizer instance with the live stream mode:
+
 def print_result(
-    result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int
+        result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int
 ):
-    print("gesture recognition result: {}".format(result))
+    if not result.hand_landmarks or not result.hand_landmarks[0]:
+        return
+
+    update_landmark_buffer(
+        result.hand_landmarks[0], output_image.width, output_image.height
+    )
 
 
 def take_picture(path: str) -> None:
@@ -24,7 +31,7 @@ def take_picture(path: str) -> None:
     while True:
         _, frame = video.read()
 
-        cv2.imshow("Face Verification", frame)
+        cv2.imshow("Hand recognizer", frame)
         if cv2.waitKey(1) & 0xFF == ord(" "):
             cv2.imwrite(path, frame)
             break
@@ -33,15 +40,12 @@ def take_picture(path: str) -> None:
     cv2.destroyAllWindows()
 
 
-def convert_landmarks_to_coordinates(
-    landmarks, width: int, height: int
-) -> list[tuple[int, int]]:
-    return [
-        (int(landmark.x * width), int(landmark.y * height)) for landmark in landmarks
-    ]
+def update_landmark_buffer(landmarks, width: int, height: int) -> None:
+    for i, landmark in enumerate(landmarks):
+        landmark_buffer[i] = int(landmark.x * width), int(landmark.y * height)
 
 
-def display_landmarks(image: np.ndarray, coords: list[tuple[int, int]]) -> None:
+def display_landmarks(image: np.ndarray) -> None:
     connections = [
         (0, 1),
         (0, 17),
@@ -67,42 +71,45 @@ def display_landmarks(image: np.ndarray, coords: list[tuple[int, int]]) -> None:
     ]
 
     for i_start, i_end in connections:
-        cv2.line(image, coords[i_start], coords[i_end], (255, 255, 255), 2)
+        cv2.line(image, landmark_buffer[i_start], landmark_buffer[i_end], (255, 255, 255), 2)
 
-    for i, (x, y) in enumerate(coords):
+    for i, (x, y) in enumerate(landmark_buffer):
         cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
         cv2.putText(
             image, f"{i}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255)
         )
 
-    while True:
-        cv2.imshow("Landmarks", image)
-        if cv2.waitKey(1) & 0xFF == ord(" "):
-            break
-
-    cv2.destroyAllWindows()
-
 
 def main():
     data_dir = Path(r".\data")
     models_dir = Path(r".\models")
-    model_path = models_dir / "gesture_recognizer.task"
+    model_path = models_dir / "hand_landmarker.task"
 
-    options = GestureRecognizerOptions(
+    options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=model_path),
-        running_mode=VisionRunningMode.IMAGE,
-        # result_callback=print_result,
+        running_mode=VisionRunningMode.LIVE_STREAM,
+        result_callback=print_result,
     )
 
-    # take_picture((data_dir / "hand.png").as_posix())
-    with GestureRecognizer.create_from_options(options) as recognizer:
-        hand = cv2.imread((data_dir / "hand.png").as_posix())
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=hand)
-        data = recognizer.recognize(mp_image)
-        coords = convert_landmarks_to_coordinates(
-            data.hand_landmarks[0], hand.shape[1], hand.shape[0]
-        )
-        display_landmarks(hand, coords)
+    video = cv2.VideoCapture(0)
+
+    with HandLandmarker.create_from_options(options) as recognizer:
+        start_time = time.time()
+        while True:
+            ret, frame = video.read()
+            timestamp = (time.time() - start_time) * 1000
+            if not ret:
+                continue
+
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+            recognizer.detect_async(mp_image, int(timestamp))
+            display_landmarks(frame)
+            cv2.imshow("Landmarks", frame)
+            if cv2.waitKey(1) & 0xFF == ord(" "):
+                break
+
+        video.release()
+        cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
