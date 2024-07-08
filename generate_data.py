de456714d7d4b2a503f90affd30ffb4bbc6bdb26
Edoardo Protti <edoardo.protti@gmail.com>
2024-07-08 22:41:07 +0200

    generate dataset array

--- generate_data.py
+++ generate_data.py
@@ -29,7 +29,8 @@ def take_picture(path: Path) -> None:
         if key == " ":
             break
         elif key in GESTURES:
-            cv2.imwrite(str(path / key / str(uuid.uuid4())), frame)
+            print(key, uuid.uuid4())
+            cv2.imwrite(str(path / key / f"{uuid.uuid4()}.png"), frame)
 
     video.release()
     cv2.destroyAllWindows()
@@ -41,14 +42,22 @@ def convert_image_to_numpy(data_dir: Path, model_path: Path) -> None:
         running_mode=VisionRunningMode.IMAGE,
     )
     coordinates = []
+    invalid_photos = []
 
     with HandLandmarker.create_from_options(options) as recognizer:
         for root, dirs, files in os.walk(data_dir):
             for file in files:
+                if file == "hands.npy":
+                    continue
                 image = cv2.imread(rf"{root}\{file}")
-                width, height = image.shape
+                width, height, _ = image.shape
                 mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
                 result = recognizer.detect(mp_image)
+
+                if not result.hand_landmarks:
+                    print(rf"No hand recognized: {root}\{file}")
+                    invalid_photos.append(rf"{root}\{file}")
+                    continue
 
                 landmarks = result.hand_landmarks[0]
                 handedness = result.handedness[0][0].display_name
@@ -61,12 +70,17 @@ def convert_image_to_numpy(data_dir: Path, model_path: Path) -> None:
                 else:
                     raise ValueError(f"Invalid handedness: {handedness}")
 
-                flattened_coords = list(chain(*coords)).append(GESTURES.index(root.split(r"/")[-1]))
+                flattened_coords = list(chain(*coords))
+                flattened_coords.append(GESTURES.index(root.split("\\")[-1]))
                 coordinates.append(flattened_coords)
 
         np.save(data_dir / "hands.npy", np.array(coordinates))
+        for photo in invalid_photos:
+            os.remove(photo)
+        print(f"Removed {len(invalid_photos)} photos of unrecognized hands")
 
 
-if __name__ == '__main__':
-    take_picture(DATA_DIR)
+if __name__ == "__main__":
+    # take_picture(DATA_DIR / "images")
     convert_image_to_numpy(DATA_DIR, MODELS_DIR / "hand_landmarker.task")
+    # print(np.load(DATA_DIR / "hands.npy").shape)
