6b15f0e3bcdd8e9527e7a36f75a95e9cfe2979df
Edoardo Protti <edoardo.protti@gmail.com>
2024-07-14 23:05:01 +0200

    renames

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
-import torch
 from pathlib import Path
 
 import cv2
 import mediapipe as mp
+import numpy as np
+import torch
 
-from callback import DataBuffer
+from buffer import DataBuffer
 from net import GestureNet
 
 BaseOptions = mp.tasks.BaseOptions
@@ -19,17 +20,17 @@ MAX_HANDS = 1
 
 
 def main():
-    buffer = DataBuffer()
+    buffer = DataBuffer(sample_size=20)
 
     options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=MODELS_DIR / "hand_landmarker.task"),
         running_mode=VisionRunningMode.LIVE_STREAM,
         num_hands=MAX_HANDS,
-        result_callback=buffer.result_callback,
+        result_callback=buffer.add_result,
     )
 
     model = GestureNet(num_gestures=10)
-    model.load_state_dict(torch.load(MODELS_DIR / "gesture_net.pth"))
+    model.load_state_dict(torch.load(MODELS_DIR / "GestureNet.pt"))
 
     video = cv2.VideoCapture(0)
 
@@ -44,6 +45,10 @@ def main():
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
             recognizer.detect_async(mp_image, int(timestamp))
             annotated_frame = buffer.display_landmarks(frame)
+            if buffer.is_full:
+                gesture = model.infer(np.array(buffer.landmarks_buffer),
+                                      np.array([1 if hand == "Left" else 0 for hand in buffer.handedness_buffer]))
+                cv2.putText(annotated_frame, gesture, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
             cv2.imshow("Landmarks", annotated_frame)
             if cv2.waitKey(1) & 0xFF == ord(" "):
                 break
