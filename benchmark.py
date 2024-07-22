97c0e61ec0cbefbaa903ac1a15daeb0da5e3e31a
edoardo-p <61146513+edoardo-p@users.noreply.github.com>
2024-07-22 17:41:55 +0200

    Updated benchmark script

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,4 @@
+import os
 import time
 from pathlib import Path
 
@@ -5,24 +6,27 @@ import torch
 
 from net import GestureNet
 
-MODELS_DIR = Path(r".\models")
+MODELS_DIR = Path(r".\experiments")
 
 
 def main():
-    model = GestureNet(num_gestures=8)
-    model.load_state_dict(torch.load(MODELS_DIR / "GestureNet.pt"))
+    for sub_dir in os.listdir(MODELS_DIR):
+        print(f"Benchmarking {sub_dir}")
+        num_landmarks = 63 if sub_dir.startswith("xyz") else 42
+        model = GestureNet(num_gestures=8, num_landmarks=num_landmarks)
+        model.load_state_dict(torch.load(MODELS_DIR / sub_dir / "GestureNet_0.pt"))
 
-    tensors = (
-        torch.randn(1, 42),
-        torch.randn(20, 42),
-        torch.randn(50, 42),
-        torch.randn(100, 42),
-    )
-    for tensor in tensors:
-        start = time.perf_counter_ns()
-        model(tensor)
-        end = time.perf_counter_ns()
-        print(f"Time: {(end - start)} ns")
+        tensors = (
+            torch.randn(1, num_landmarks),
+            torch.randn(20, num_landmarks),
+            torch.randn(50, num_landmarks),
+            torch.randn(100, num_landmarks),
+        )
+        for tensor in tensors:
+            start = time.perf_counter_ns()
+            model(tensor)
+            end = time.perf_counter_ns()
+            print(f"Time: {(end - start)} ns")
 
 
 if __name__ == "__main__":
